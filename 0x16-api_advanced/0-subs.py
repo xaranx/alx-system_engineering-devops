2149658bc2a5bbab3640675fffe1738cb3b8985e
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2024-01-16 15:14:35 -0800

    task 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
-        "User-Agent": "0x16. API_advanced"
+        "User-Agent": "0x16. API_advanced-e_kiminza"
         }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
